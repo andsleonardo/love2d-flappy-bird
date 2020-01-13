8d82e8d9c918aa1d5adc9929770f9d820efcf24c
Anderson Leonardo <andsnleo@gmail.com>
2020-01-12 21:56:12 -0300

    Add fonts, create ui table to store them

---
+++ src/ui.lua
@@ -0,0 +1,16 @@
+local ui = {}
+
+local function createFonts()
+  local G = love.graphics
+
+  return {
+    small = G.newFont("assets/fonts/monogram_extended.ttf", 16),
+    normal = G.newFont("assets/fonts/monogram_extended.ttf", 22),
+    large = G.newFont("assets/fonts/monogram_extended.ttf", 30),
+    title = G.newFont("assets/fonts/flappy.ttf", 42)
+  }
+end
+
+ui.fonts = createFonts()
+
+return ui
